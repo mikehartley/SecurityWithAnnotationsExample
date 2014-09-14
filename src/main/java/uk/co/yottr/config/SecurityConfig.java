2e4e21aa71dde37efac7b0b6b030d16d8d8e9d19
Mike Hartley <mikehartley@users.noreply.github.com>
2014-09-14 19:26:19 +0100

    added more to pom, still seems to deploy

--- src/main/java/uk/co/yottr/config/SecurityConfig.java
+++ src/main/java/uk/co/yottr/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package uk.co.yottr.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,19 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication().withUser("mkyong").password("123456").roles("USER");
-//		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
-//		auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-//		http.authorizeRequests()
-//			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-//			.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
-//			.and().formLogin();
 
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
@@ -39,4 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
 	}
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
